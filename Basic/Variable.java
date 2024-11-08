ede081fd52c548b6dd331f23a887f3829e397e4d
Saiful <saifulislam.cse164@gmail.com>
2024-11-08 09:27:46 +0100

    variables in java

---
+++ Basic/Variable.java
@@ -0,0 +1,10 @@
+package Basic;
+
+public class Variable {
+    public static void main(String[] args) {
+        int x = 8;
+        int y = 9;
+        System.out.println(6);
+        System.out.println(x+y);
+    }
+}
