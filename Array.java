55efa4bca643ee29cead1aa39efb2796f26d29a8
Saiful <saifulislam.cse164@gmail.com>
2024-11-12 00:28:31 +0100

    Multidimensional array

--- Array.java
+++ Array.java
@@ -2,6 +2,17 @@ public class Array {
     public static void main(String[] args) {
         int num [] = {12, 2, 6};
         int numbers [] = new int[4];
+        numbers[0]= 4;
+        numbers[1]= 5;
+        numbers[2]= 6;
+        numbers[3]= 7;
+
+        num[1] = 6;
         System.out.println(num[1]);
+
+        for( int i = 0; i <= 3; i++){
+            System.out.println(numbers[i]);
+        }
+
     }
 }
---
+++ Multidimentional_array.java
@@ -0,0 +1,36 @@
+public class Multidimentional_array {
+    public static void main(String[] args) {
+        int number[][] = new int[3][4];
+
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 3; j++) {
+                int random = (int) (Math.random() * 100);
+                System.out.print(random + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 3; j++) {
+                number[i][j] = (int) (Math.random() * 100);
+                System.out.print(number[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 3; j++) {
+                System.out.print(number[i][j] + " ");
+            }
+            System.out.println();
+        }
+System.out.println();
+        for( int[] n : number){
+            for(int element : n){
+                System.out.print(element + "  ");
+            }
+            System.out.println();
+        }
+    }
+}
