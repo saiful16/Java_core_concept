c1d25d659796715caa5d9b8eb740b416984ae0d9
Saiful <saifulislam.cse164@gmail.com>
2024-11-11 23:15:42 +0100

    array

---
+++ Array.java
@@ -0,0 +1,7 @@
+public class Array {
+    public static void main(String[] args) {
+        int num [] = {12, 2, 6};
+        int numbers [] = new int[4];
+        System.out.println(num[1]);
+    }
+}
