da4dbfe82fb049c12c7d233cf0d79981eb4b21b7
saiful16 <saifulislam.cse164.com>
2024-12-11 15:55:54 +0100

    equal to three decimal

---
+++ EqualByThreeDecimalPlaces.java
@@ -0,0 +1,13 @@
+public class EqualByThreeDecimalPlaces {
+
+    public static boolean areEqualByThreeDecimalPlaces(double number1, double number2) {
+
+        long firstRound = (long) (number1 * 1000);
+        long secondRound = (long) (number2 * 1000);
+        return firstRound == secondRound;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(areEqualByThreeDecimalPlaces(23.456,23.457));
+    }
+}
